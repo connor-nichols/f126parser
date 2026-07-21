97a80e4e1ac88724e80df80a319c77f255ecefa6
cnichols <cnichols9@huskers.unl.edu>
2026-07-20 21:13:52 -0500

    begin neopixel work

--- pi-hardware-main.py
+++ pi-hardware-main.py
@@ -42,7 +42,7 @@ num_pixels = 16
 ORDER = neopixel.GRBW
 
 pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+    pixel_pin, num_pixels, brightness=0.1, auto_write=False, pixel_order=ORDER
 )
 
 
